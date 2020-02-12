e8d23ed9cc29903eafd4c4c18189e832a1eab39e
zj <1204249255@qq.com>
2020-02-12 18:25:01 +0800

    添加公共组件

--- pages/home/Home.js
+++ pages/home/Home.js
@@ -11,13 +11,13 @@ Page({
   },
   // 点击 + 号
   addButton() {
-    this.setData({
+    this.setData({  // 逻辑层刷新，渲染到视图页面上
       num: this.data.num+1
     })
   },
   // 点击 - 号
   cantenBotton() {
-    this.setData({
+    this.setData({ // setData函数主要用于将逻辑层数据发送到视图层，同时对应的改变this.data.x的值。
       num: this.data.num-1
     })
   },
