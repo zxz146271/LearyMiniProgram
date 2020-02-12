59e6749483f82a7e6431400ca20a5d80a9f77e1a
zj <1204249255@qq.com>
2020-02-12 16:18:04 +0800

    数据绑定，数组循环列表渲染，点击事件bindtap，this.setData 的使 用

--- pages/home/Home.js
+++ pages/home/Home.js
@@ -1,1 +1,24 @@
-Page({})
+Page({
+  data: {
+    name: '测试绑定数据',
+    studuts: [
+      { id: 0, name: '马克1号' },
+      { id: 1, name: '马克22号' },
+      { id: 2, name: '马克42号' },
+      { id: 3, name: '马克99号'},
+    ],
+    num: 0
+  },
+  // 点击 + 号
+  addButton() {
+    this.setData({
+      num: this.data.num+1
+    })
+  },
+  // 点击 - 号
+  cantenBotton() {
+    this.setData({
+      num: this.data.num-1
+    })
+  },
+})
